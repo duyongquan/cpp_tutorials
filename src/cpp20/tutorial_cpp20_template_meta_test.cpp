9ed708cddeb29575dd772b38fcb913befaccf76b
duyongquan <quandy2020@126.com>
2023-03-24 21:18:43 +0800

    [Fix] requires cpp20 demos
    	1 requires cpp20 demos
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- src/cpp20/tutorial_cpp20_template_meta_test.cpp
+++ src/cpp20/tutorial_cpp20_template_meta_test.cpp
@@ -5,7 +5,25 @@
 namespace tutorials{
 namespace cpp20 {
 
+template<typename T>
+concept Comparable = requires(T a, T b)
+{
+    {a, b} -> std::same_as<bool>;
+};
 
+template<typename T>
+requires Comparable<T>
+T max(T a, T b)
+{
+    return a > b ? a : b;
+}
+
+struct Foo {};
+
+void test()
+{
+    max(Foo{}, Foo{});
+}
 
 }  // 
 }  // namespace tutorial
